9093bd96078015240e67694ead8b782c6a0dc2a0
Zani231089 <231089@virtualwindow.co.za>
2023-05-07 13:45:24 +0200

    Final

---
+++ js/index.js
@@ -0,0 +1,12 @@
+let numOne = 12;
+let numTwo = 20;
+let numThree = 4;
+let numFour = 0;
+
+let teamTotal = numOne + numTwo;
+let victory = teamTotal / numThree;
+let total = victory * numFour;
+
+let answer = "The answer is " + total;
+
+console.log(answer)
